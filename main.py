9b486ea64664fe06a3ccbeca7b1db3fdaf63035b
poszlaka <sebastian.kt7@gmail.com>
2016-11-14 12:37:47 +0100

    add def getch

--- main.py
+++ main.py
@@ -14,6 +14,22 @@ def drukowanie_tablicy(lista):
     for i in lista:
         print(''.join(i))
 
+def move(list):
+    pass
+
+def getch():
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+x = getch()
+print(x)
 
 def main():
     drukowanie_tablicy(tablica(15, 10))
