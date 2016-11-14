3eccd6d5b6086ff2c7425023fb6876d255333227
Stefan Kania <stefankania95@gmail.com>
2016-11-14 11:54:47 +0100

    table

---
+++ main.py
@@ -0,0 +1,18 @@
+def tablica(x=5, y=5):
+    lista = []
+    for rzad in range(x):
+        lista.append([])
+        for kolumn in range(y):
+            if rzad == 0 or rzad == x-1 or kolumn == 0 or kolumn == y-1:
+                lista[rzad].append('x')
+            else:
+                lista[rzad].append('.')
+    return lista
+
+
+def drukowanie_tablicy(lista):
+    for i in lista:
+        print(''.join(i))
+
+
+drukowanie_tablicy(tablica(15,10))
