6391133e5ee6a3191d9e3929561345bb37ffdbfb
komuher <komuher@gmail.com>
2016-11-15 14:51:50 +0100

    npc revork

--- npc.py
+++ npc.py
@@ -1,31 +1,40 @@
+import inventory
 boss = "!"
 wife = "B"
 host = "O"
+menel = "&"
 
 
-def host(host_x, host_y, player_x, player_y):
-    while
-        if player_x == host_x -1 or player_y == host_y -1:  # change in move
-            input("press e to talk: ")
-            if x < 1
+def menel():
+    while backpack['food'] > 20:
+        menelll = input("Wanna change 20 food for 2 vodka? / write Y to accept or N for reject ")
+
+        if menelll = ""
+
+def host():
+    while backpack['clothes'] < 2:
+        if #player_x == host_x -1 or player_y == host_y -1:  # change in move
+            print("press e to talk: ")
+            x = 0
+            if x < 1:
                 name = input("Hello what's ur name? ")
                 x += 1
-            if backpack['clothes'] > 2:
+            if backpack['clothes'] < 2:
                 print("{} u need to find ur clothes before i can let u go ".format(imie))
-            elif backpack['clothes'] == 2:
+            elif backpack['clothes'] >= 2:
                 print("I wish u good luck byeee ")
 
-def wife(wife_x, wife_y):
+def wife():
     while backpack['food'] < 100:   # tutaj dac pozycje szopy i ona z niej wychodzi
         if player_x == wife_x +1:
             print("U f***** drunkard what have u done last night?!! ")
             print("So u dont wanna to talk?! So u need to gather 100  corn cob ")
 
-def boss(boss_x, boss_y):
+def boss():
     while backpack["vodka"] < 2:
-        negotiate = input("if u wanna negotiate press E, if u wanna sell print Q ")
+        negotiate = input("if u wanna negotiate write N, if u wanna sell write Q ")
         negotiate = negotiate.lower()
-        if negotiate == "e":
+        if negotiate == "n":
             print("You need to bring me vodka if wanna to negotiate with me  ")
             break
         elif negotiate == "q":
