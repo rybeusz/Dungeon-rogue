8d8af7f429fcd5c4975bce5ea82e07e21a9498e6
Stefan Kania <stefankania95@gmail.com>
2016-11-15 13:11:18 +0100

    wheat and farm

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ def board(x, y, player_x, player_y):
     return list
 
 
-
 def show_board(list):
     for i in list:
         print(''.join(i))
@@ -38,20 +37,74 @@ def random_item(list):
     return list
 
 
-def main():
-    player_x = 3
-    player_y = 3
-    game_board = board(20, 60, player_x, player_y)
+def random_buildings(game_board, level):
+    random_area = [random.randrange(0, 11, 10), random.randrange(0, 31, 30)]
+    if level == 1:
+        x = random_area[1]
+        y = random_area[0]
+        for i in range(y+3, y+8):
+            for z in range(x+7, x+19):
+                if i == y+3 or i == y+7 or z == x+7 or z == x+18:
+                    game_board[i][z] = '#'
+        game_board[y+7][x+5+7] = '.'  # tavern doors
+        game_board[y+7][x+6+7] = '.'  # tavern doors
+    if level == 2:
+        x = random_area[1]
+        y = random_area[0]
+        for i in range(y+4, y+8):
+            for z in range(x+4, x+19):
+                if i == y+4 or i == y+7 or z == x+4 or z == x+18:
+                    game_board[i][z] = '#'
+        game_board[y+7-1][x+4] = '.'
+
+        while x == random_area[1] and y == random_area[0]: # random area for wheat
+            x = random.randrange(0, 31, 30)
+            y = random.randrange(0, 11, 10)
+
+        for i in range(y+2, y+8):
+            for z in range(x+3, x+27):
+                    game_board[i][z] = '/'
+
+
+    if level == 3:
+        pass
+    return game_board
+
+
+def levels(level):
     inventory = 0
-    game_board = random_item(game_board)
+    # level 1
+    if level == 1:
+        player_x = 3
+        player_y = 3
+        game_board = board(20, 60, player_x, player_y)
+        game_board = random_item(game_board)
+        game_board = random_buildings(game_board, level)
+    # level 2
+    if level == 2:
+        player_x = 1
+        player_y = 1
+        game_board = board(20, 60, player_x, player_y)
+        #game_board = random_item(game_board)
+        game_board = random_buildings(game_board, level)
+    # level 3
+    if level == 3:
+        pass
+
     while True:
         os.system('clear')
         show_board(game_board)
-        zwrot = movement(game_board, player_x, player_y, inventory)
-        game_board = zwrot[0]
-        player_x = zwrot[1]
-        player_y = zwrot[2]
-        inventory = zwrot[3]
+        move_variables = movement(game_board, player_x, player_y, inventory)
+        game_board = move_variables[0]
+        player_x = move_variables[1]
+        player_y = move_variables[2]
+        inventory = move_variables[3]
+
+
+def main():
+    #levels(1)
+    levels(2)
+    levels(3)
 
 
 if __name__ == "__main__":
