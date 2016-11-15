dd5753a12df923f085c4508da0007cc572c5504a
poszlaka <sebastian.kt7@gmail.com>
2016-11-15 16:44:59 +0100

    finish inventory and add csv files

--- inve.py
+++ inve.py
@@ -1,3 +1,8 @@
+import string
+
+
+# inv must be empty before import
+
 def display_inventory(inventory):
     """prints keys and values of inventory"""
     weight = []
@@ -14,15 +19,10 @@ def display_inventory(inventory):
 
 def add_to_inventory(inventory, loot):
     """ adding items from lists to inventory"""
-    #inv = {'rope': [1, 'other', 12], 'torch': [6, 'other', 12], 'gold coin': [42, 'other', 42],
-    #       'dagger': [1, 'weapon', 2],
-    #       'arrow': [12, 'weapon', 12]}
-    #loot = [['gold coin', 'other', 1], ['dagger', 'weapon', 2], ['gold coin', 'other', 1], ['gold coin', 'other', 1],
-    #        ['ruby', 'other', 1]]
     for item in loot:
         if item[0] in inventory.keys():
-            inventory[item[0]][0] += 1 # amount
-            inventory[item[0]][2] += item[2] # weight
+            inventory[item[0]][0] += 1  # amount
+            inventory[item[0]][2] += item[2]  # weight
         else:
             inventory[item[0]] = [1, item[1], item[2]]
     return inventory
@@ -48,56 +48,44 @@ def print_table(inventory):
     print('Total weight: {}'.format(sum(weight)))
 
 
-def import_inventory(inventory, filename='import_inventory.csv'):
-    """import data from file"""
-    import_file = open(filename, 'r')
-    file = [list(line) for line in import_file]
-    for item in file:
-        for x in item:
-            if x == ',':
-                item_file = ''.join(item[:item.index(x)])  # its value
-                if item[-1] == '\n':
-                    if item_file in inventory.keys():
-                        inventory[item_file] += float(''.join(item[item.index(x) + 1:-1]))
-                    else:
-                        try:
-                            float(''.join(item[item.index(x) + 1:-1]))
-                            inventory[item_file] = float(''.join(item[item.index(x) + 1:-1]))
-                        except ValueError:
-                            pass
-                else:
-                    if item_file in inventory.keys():
-                        inventory[item_file] += float(''.join(item[item.index(x) + 1:]))
-                    else:
-                        try:
-                            float(''.join(item[item.index(x) + 1:]))
-                            inventory[item_file] = float(''.join(item[item.index(x) + 1:]))
-                        except ValueError:
-                            pass
+def fastsplit(line):
+    """Split line from imported file"""
+    line = [l.strip().split(',') for l in line]  # strip() remove \n and stuff like this
+    return line
+
+
+def import_inventory(inventory, filename):
+    """Importing inventory from file and merging with current inventory"""
+    file_loot = open(filename, 'r')
+    loot = fastsplit(file_loot)
+    for item in loot:
+        inventory[item[0]] = [int(item[1]), item[2], int(item[3])]
+    print(loot)
+    return inventory
 
 
 def export_inventory(inventory, filename='export_inventory.csv'):
     """export data to file"""
-    if filename is 'export_inventory.csv':
-        export_file = open('export_inventory.csv', 'w+')
-        for key, value in inventory:
-            export_file.write('{},{}'.format(key, value))
-    else:
-        export_file = open(filename, 'w+')
-        for key, value in inventory.items():
-            export_file.write('{},{}\n'.format(key, value))
-
+    export_file = open(filename, 'w+')
+    for key, value in inventory.items():
+            export_file.write('{},{},{},{}\n'.format(key, value[0],value[1],value[2]))
 
 def main():
     """check all def"""
-    inv = {'rope': [1, 'other', 12], 'torch': [6,'other',12], 'gold coin': [42, 'other', 42], 'dagger': [1,'weapon', 2],
+    inv = {'rope': [1, 'other', 12], 'torch': [6, 'other', 12], 'gold coin': [42, 'other', 42],
+           'dagger': [1, 'weapon', 2],
            'arrow': [12, 'weapon', 12]}
-    loot = [['gold coin', 'other', 1], ['dagger', 'weapon', 2], ['gold coin', 'other', 1], ['gold coin', 'other', 1], ['ruby', 'other', 1]]
-    display_inventory(inv)
-    add_to_inventory(inv, loot)
-    display_inventory(inv)
+    loot = [['gold coin', 'other', 1], ['dagger', 'weapon', 2], ['gold coin', 'other', 1], ['gold coin', 'other', 1],
+            ['ruby', 'other', 1]]
+    # display_inventory(inv)
+    # add_to_inventory(inv, loot)
+    # display_inventory(inv)
     print(inv)
-    print_table(inv)
-    import_inventory(inv)
+    # print_table(inv)
+    #import_inventory(inv, 'import_inventory.csv')
+    display_inventory(inv)
+    export_inventory(inv)
+
+
 if __name__ == '__main__':
     main()
