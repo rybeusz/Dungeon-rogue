c9edd925002a092cd7efafe8491965e3eafcdfb8
komuher <komuher@gmail.com>
2016-11-16 13:39:47 +0100

    npc upgrade

--- npc.py
+++ npc.py
@@ -1,60 +1,70 @@
 import inve
+import guess
 boss = "!"
 wife = "B"
 host = "O"
 menel = "&"
-backpack = {}
 
-def menel():
-    if "wheat" in backpack:
-        if backpack['wheat'][0] > 20:
-            menelll = input("Wanna change 20 food for 2 vodka? / write Y to accept or N for reject ")
 
-            if menelll = ""
+def menel(backpack):
+    loot = [["vodka", "food", 1], ["gold coin", "other", 1]]
+    if "gold coin" in backpack:
+        if backpack['gold coin'][0] > 10:
+            vodka_sell = input("Wanna change 10 gold coins for 1 vodka? / write yes to accept or no for reject")
+            if vodka_sell == "yes":
+                inve.remove_item(backpack, loot[1], 10)
+                inve.add_to_inventory(backpack, loot[0], 1)
+            if vodka_sell == "no":
+                print("bye ")
+            else:
+                print("u need to write yes or no ")
+        else:
+            print("u must have some money ")
+
     else:
         print("elo")
 
-def host(name):
+
+def host2(backpack, name):
     namei = str(name)
     if "clothes" not in backpack:
-        print("{} ubierz sie!".format(namei))
+        print("{} dress up".format(namei))
     else:
-        print("")
+        print("U look like a badass ")
 
 
-def wife(): # ogarnąć ta ku.... tylko pierwsze podejsice
+def wife2(backpack):  # ogarnąć ta ku.... tylko pierwsze podejsice
     if "scythe" and "corn" in backpack:
-        if backpack['corn'][0] < 100:
-            print("You need to gather 100  corn cob get back to work ")
+        if backpack['corn'][0] < 20:
+            print("You need to gather 20 corn cob so get back to work! ")
         else:
-            print("DZIEKI ZIa")
+            print("Thanks u can go now ")
+
+
+def wife1(backpack):
+    if "corn" not in backpack:
+        print("Where have u been u f...... drunktard, get back to work and collect 20 corn cobs! ")
+
 
 def trader(backpack):
+    loot = [["corn", "food", 1]]
     if "corn" in backpack:
         x = input("Do u wanna sell some corn mate? write yes or no: ")
         x = x.lower()
         if x == "yes":
-            z = int(input("How much u wanna sell?: "))
-                if z > backpack["corn"][0]:
-                    print("u dont have that much corn in ur backpack ")
-                else:
-                    inve.remove_item
-
-
-
-def boss():
-    if "vodka" not in backpack:
-        negotiate = input("if u wanna negotiate write N, if u wanna sell write Q ")
-        negotiate = negotiate.lower()
-        if negotiate == "n":
-            print("You need to bring me vodka if wanna to negotiate with me  ")
-        if negotiate == "q":
-            print("GJ u win the game")  # + dodac okreslana liczbe kasy
-            quit()
+            remove_corn = int(input("How much u wanna sell?: "))
+            if remove_corn > backpack["corn"][0]:
+                print("u dont have that much corn in ur backpack ")
+            else:
+                inve.remove_item(backpack, loot, remove_corn)
+        elif x == "no":
+            print("Come to me when u wanna sell corn ")
+        else:
+            print("ur answer need to be yes or no ")
     else:
-        input("So how much u wanna for rearaeare")
+        print("You dont have any corn come when u get some ")
 
 
-
-
-
+def boss(backpack):
+    if "vodka" not in backpack:
+        guess.main()
