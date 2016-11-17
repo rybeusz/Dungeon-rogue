a7bea25eaf7cf3df3bbf701cba16ccb86f050c69
komuher <komuher@gmail.com>
2016-11-17 11:04:08 +0100

    vodka

--- npc.py
+++ npc.py
@@ -15,20 +15,22 @@ def menel(backpack):
                 inve.remove_item(backpack, loot[1], 10)
                 inve.add_to_inventory(backpack, loot[0], 1)
             if vodka_sell == "no":
-                print("bye ")
+                print("bye come to papa again :0 ")
+                enter()
             else:
-                print("u need to write yes or no ")
+                print("You need to write / yes or no ")
+                enter()
         else:
-            print("u must have some money ")
-
+            print("You must have at least 10 to buy stuff from me! ")
+            enter()
     else:
-        print("elo")
+        print("Come back when u earn some money! ")
+        enter()
     return backpack
 
 
 def host2(backpack, name="Mariusz"):
     namei = str(name)
-    text = ''
     if "clothes" not in backpack:
         print("{} dress up".format(namei))
         enter()
@@ -40,8 +42,10 @@ def wife2(backpack):  # ogarnąć ta ku.... tylko pierwsze podejsice
     if "scythe" and "corn" in backpack:
         if backpack['corn'][0] < 20:
             print("You need to gather 20 corn cob so get back to work! ")
+            enter()
         else:
             print("Thanks u can go now ")
+            enter()
 
 
 def wife1(backpack):
@@ -53,7 +57,7 @@ def trader(backpack):
     loot2 = [["gold coin", "other", 1]]
     loot = [["corn", "food", 1]]
     if "corn" in backpack:
-        x = input("Do u wanna sell some corn mate? \n write yes or no: ")
+        x = input("Do u wanna sell some corn mate?\n write yes or no: ")
         x = x.lower()
         if x == "yes":
             remove_corn = int(input("How much u wanna sell?: "))
@@ -65,14 +69,18 @@ def trader(backpack):
         elif x == "no":
             print("Come to me when u wanna sell corn ")
         else:
-            print("ur answer need to be yes or no ")
+            print("Your answer need to be yes or no ")
     else:
-        print("You dont have any corn come when u get some ")
+        print("You dont have any corn, come to me when u get some ")
+        enter()
     return backpack
 
 
 def boss(backpack):
+    loot = [["vodka", "food", 1]]
     if "vodka" in backpack:
-        guess.main()
+        guess.main(backpack["vodka"][0])
+        inve.remove_item(backpack, loot, backpack["vodka"][0])
     else:
-        print("u need to have a vodka to play with me! ")
+        print("u need to have a vodka to play with me boy! ")
+        enter()
