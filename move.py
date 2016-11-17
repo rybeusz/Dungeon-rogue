09c8d556f680def718b1c54d717fc86fcde650e8
Stefan Kania <stefankania95@gmail.com>
2016-11-17 13:38:14 +0100

    move c

--- move.py
+++ move.py
@@ -2,7 +2,7 @@ import npc
 import inve
 
 
-def movement(list, player_x, player_y, inv):
+def movement(game_board, player_x, player_y, inv):
     # black magic
     import sys
     import tty
@@ -18,23 +18,25 @@ def movement(list, player_x, player_y, inv):
     # end of black magic
     key = {"w": (-1, 0), "s": (1, 0), "a": (0, -1), "d": (0, 1)}
     if x in ("w", "s", "a", "d"):
-        if list[player_x + key[x][0]][player_y + key[x][1]] == chr(182):  # collecting wheat
+        pos = game_board[player_x + key[x][0]][player_y + key[x][1]]
+        if pos == chr(182):  # collecting wheat
             corn = [['corn', 'food', 1]]
             inve.add_to_inventory(inv, corn, 1)
-        if list[player_x + key[x][0]][player_y + key[x][1]] in ('a', 'b', 'c', 'd', 'e'):  # collecting clothes
+        if pos in ('a', 'b', 'c', 'd', 'e'):  # collecting clothes
             cloth = [['clothes', 'other', 1]]
             inve.add_to_inventory(inv, cloth, 1)
-        if list[player_x + key[x][0]][player_y + key[x][1]] not in special_char:
-            list[player_x][player_y] = "."
-            list[player_x + key[x][0]][player_y + key[x][1]] = "@"
+        if pos in ["O", '❤', "☠", "♏", '¢']:  # npc functions
+            npc_dict = {"O": 'host', '❤': 'wife1', "☠": 'boss', "♏": 'bum', '¢': 'trader'}
+            getattr(npc, npc_dict[list[player_x + key[x][0]][player_y + key[x][1]]])(inv)
+        if pos not in special_char:  # moving
+            game_board[player_x][player_y] = "."
+            game_board[player_x + key[x][0]][player_y + key[x][1]] = "@"
             player_x += key[x][0]
             player_y += key[x][1]
-        if list[player_x + key[x][0]][player_y + key[x][1]] in ["O", '❤', "☠", "♏", '¢']:
-            npc_dict = {"O": 'host2', '❤': 'wife1', "☠": 'boss', "♏": 'menel', '¢': 'trader'}
-            getattr(npc, npc_dict[list[player_x + key[x][0]][player_y + key[x][1]]])(inv)
+
 
     if x == "x":
         print(inv)
         quit()
 
-    return list, player_x, player_y, inv
+    return game_board, player_x, player_y, inv
